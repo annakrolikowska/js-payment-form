154fe3e65a5aa4c3fd08a54d0bf8776464b54440
Anna <krolikowska.ann@gmail.com>
2023-04-18 13:31:20 +0200

    added form validaton

--- src/pages/home-page/scripts/home-page.js
+++ src/pages/home-page/scripts/home-page.js
@@ -1,4 +1,35 @@
-/// Homepage JavaScript File
-/// Here we import all the JavaScript files we need for our homepage.
-
 import '../styles/home-page.scss'
+
+const form = document.querySelector('.home-page__form');
+const submitBtn = form.querySelector('.home-page__button');
+const inputs = form.querySelectorAll('input');
+
+function validateInputs() {
+  let isValid = true;
+  inputs.forEach(input => {
+    if (!input.checkValidity()) {
+      isValid = false;
+    }
+  });
+  return isValid;
+}
+
+inputs.forEach(input => {
+  input.addEventListener('change', () => {
+    if (validateInputs()) {
+      submitBtn.disabled = false;
+      console.log('kitek')
+    } else {
+      submitBtn.disabled = true;
+      console.log('pawcio')
+    }
+  });
+});
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  if (validateInputs()) {
+    window.alert("Success")
+    form.submit();
+  }
+});
